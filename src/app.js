72ced36f421386996dfe1695b40b77596f08a546
ZekeriyaOtumlu <zekyotumlu@hotmail.com>
2022-10-15 15:25:07 +0000

    app.js updated

--- src/app.js
+++ src/app.js
@@ -9,3 +9,22 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+let finalArray = [];
+
+function domainExecute() {
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        // console.log(pronoun[p] + " " + adj[a] + " " + noun[n]);
+        finalArray.push(pronoun[p] + "" + adj[a] + "" + noun[n] + ".com");
+      }
+    }
+  }
+  return finalArray;
+}
+// console.log(domainExecute().join("\n"));
+document.querySelector("#page").innerHTML = domainExecute().join("\n");
